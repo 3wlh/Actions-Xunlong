80058ae32432aa58614dfb28326d849039c29234
3wlh <1094890624@qq.com>
2024-10-10 19:38:21 +0800

    up

---
+++ diy-part2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Function to apply common patches
+apply_common_patches() {
+  local dir=$1
+  if [ -d "$dir" ]; then
+    sed -i '1i#include <linux/completion.h>' "$dir/include/osdep_service_linux.h"
+    sed -i '/thread_exit()/a return 0;' "$dir/core/rtw_cmd.c"
+  fi
+}
+
+# List of directories to apply patches (add more directories as needed)
+dirs=(
+  "/workdir/openwrt/package/kernel/rtl8812au-ct/src"
+  "/workdir/openwrt/package/kernel/mt76/src" # Example for mt76 driver
+  # Add other directories here
+)
+
+# Apply patches to all listed directories
+for dir in "${dirs[@]}"; do
+  apply_common_patches "$dir"
+done
+
+# 添加luci-theme-argon
+(git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon && {
+    [ -d package/luci-theme-argon ] && echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config 
+}) 
+# 添加luci-app-argon-config
+(git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && {
+    [ -d package/luci-app-argon-config ] && echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config 
+})
